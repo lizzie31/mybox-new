9e51e497d3f45bb1fbd91ce8388fac5184a9f633
nofargido <nofargido90@walla.com>
2015-06-10 16:39:10 +0300

    createnewgroup

--- ass3/src/controllers/createNewGroupController.java
+++ ass3/src/controllers/createNewGroupController.java
@@ -3,18 +3,23 @@ package controllers;
 import view.*;
 import Model.*;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import client.myboxapp;
 
-public class createNewGroupController {
+
+public class createNewGroupController extends AbstractTransfer {
 	/**
 	 * the new group that administrarotor field.
 	 */
 	private interestGroups newgroup;
 	private createNewGroupGUI group;
 	private administratorMenuController adm;
+	/**
+	 * contain the details about the new group to send the server.
+	 */
+	Envelope en;
 	public createNewGroupController(createNewGroupGUI group, administratorMenuController lastCon)
 	{
 		this.group=group;
@@ -32,9 +37,11 @@ public class createNewGroupController {
 	}
 	
 	private void buttonaddPressed() {
-		
+		newgroup= new interestGroups(null);
 		newgroup.setGroupName(group.getGroupname().getText());
-		
+		en=new Envelope(newgroup,"add new group to DB");
+		sendToServer(en);
+		myboxapp.clien.setCurrObj(this);
 		group.close();
 		adm.getAdminCon().setVisible(true);
 	
@@ -53,5 +60,11 @@ public class createNewGroupController {
 		adm.getAdminCon().setVisible(true);
 	
 	}
+
+	public createNewGroupGUI getGroup() {
+		return group;
+	}
+	
+	
 
 }
